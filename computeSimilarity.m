2e1ea5bdd3a1f25e89dbb8c60f7dcc8fbe88b3b1
DLAI007 <bjyotidutta@gmail.com>
2011-12-15 19:45:00 +0800

    Compute Similarity algorithm

--- computeSimilarity.m
+++ computeSimilarity.m
@@ -1,4 +1,4 @@
 function similarity = computeSimilarity(features1, features2)
     % Compute cosine similarity
     similarity = dot(features1, features2) / (norm(features1) * norm(features2));
-end
+end% Compute Similarity implementation
